dae2c0ec08c4998bd549f7ad72845e6b5184c884
SaifAlidevs <saif.ali@devsinc.com>
2022-06-20 12:48:07 +0500

    *UserMailer Error Resolving 1

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 #require "will_paginate"
+
+config.action_mailer.default_url_options = { :host => 'https://fierce-ravine-71442.herokuapp.com' }
